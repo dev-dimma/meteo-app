77de90a76b5076bf23db2dd4680d0567a454286a
DIMMAS-LAPTOP\Dimma Joel <dimmajoel@gmail.com>
2025-01-07 14:58:47 +0100

    Changed function value

--- src/index.js
+++ src/index.js
@@ -56,4 +56,4 @@ function handleSearchSubmit(event) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-searchCity("Paris");
+searchCity("Amsterdam");
